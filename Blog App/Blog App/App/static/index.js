38b03836316bc112bbb9a9ac0281325027b50c0f
Osylisor <93229146+Osylisor@users.noreply.github.com>
2022-08-20 00:44:10 +0200

    Delete Blog App/Blog App directory

--- Blog App/Blog App/App/static/index.js
+++
@@ -1,8 +0,0 @@
-const menuBar = document.querySelector('.menu');
-const navList = document.querySelector('.nav-list');
-
-
-menuBar.addEventListener('click', ()=>{
-
-    navList.classList.toggle('active');
-});
